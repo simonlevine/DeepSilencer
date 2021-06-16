aee1269f544254d48f2e662cce003133038317fa
Simon Levine <50503513+simonlevine@users.noreply.github.com>
2021-06-16 13:07:43 -0700

    Update DeepSilencer.py

--- code/DeepSilencer.py
+++ code/DeepSilencer.py
@@ -11,6 +11,7 @@ class DeepSilencer():
     def __init__(self,L = 200,K = 5):
         input_seq  = Input(shape=(4, L, 1))
         input_kmer = Input(shape=(4**K,))
+
         seq_conv1_ = Convolution2D(64, 4, 10, activation='relu',border_mode='valid',dim_ordering='tf')
         seq_conv1  = seq_conv1_(input_seq)
         seq_conv2_ = Convolution2D(32, 1, 3, activation='relu',border_mode='same')
@@ -30,6 +31,7 @@ class DeepSilencer():
         dense3  = Dense(8, activation='relu')(merge)
         pred_output = Dense(1, activation='sigmoid')(dense3)
         self.model = Model(input=[input_seq, input_kmer], output=[pred_output])
+
     def fit(self,train_data,train_data_kmer,train_label,filename = 'kmer_seq.h5',learning_rate = 1e-3):
         early_stopping = EarlyStopping(monitor='val_acc', verbose=0, patience=30, mode='max')
         save_best = ModelCheckpoint(filename, save_best_only=True, save_weights_only=True)
