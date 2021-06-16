645835fd4c3abcfe74cef3878f7348063b3bef21
Simon Levine <50503513+simonlevine@users.noreply.github.com>
2021-06-16 14:00:08 -0700

    add

--- code/DeepSilencer.py
+++ code/DeepSilencer.py
@@ -12,6 +12,7 @@ class DeepSilencer():
         input_seq  = Input(shape=(4, L, 1))
         input_kmer = Input(shape=(4**K,))
 
+
         seq_conv1_ = Convolution2D(64, 4, 10, activation='relu',border_mode='valid',dim_ordering='tf')
         seq_conv1  = seq_conv1_(input_seq)
         seq_conv2_ = Convolution2D(32, 1, 3, activation='relu',border_mode='same')
@@ -20,16 +21,22 @@ class DeepSilencer():
         seq_conv7_ = Convolution2D(8, 1, 3, activation='relu',border_mode='same')
         seq_conv7  = seq_conv7_(seq_pool1)
         seq_pool2  = MaxPooling2D(pool_size=(1, 2))(seq_conv7)
+
+
+        
         x = Flatten()(seq_pool2)
         dense_seq  = Dense(32, activation='relu')(x)
 
         dense1_    = Dense(64, activation='relu')
         dense1     = dense1_(input_kmer)
+
         dense_kmer = Dense(8, activation='relu')(dense1)
 
         merge   = concatenate([dense_seq, dense_kmer], axis = 1)
+
         dense3  = Dense(8, activation='relu')(merge)
         pred_output = Dense(1, activation='sigmoid')(dense3)
+
         self.model = Model(input=[input_seq, input_kmer], output=[pred_output])
 
     def fit(self,train_data,train_data_kmer,train_label,filename = 'kmer_seq.h5',learning_rate = 1e-3):
